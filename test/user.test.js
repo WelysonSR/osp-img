be4bffa394f53d75d6307ae3333a1cf85a6e8fc8
Welyson Dos Santos Rosa <welysonsr@gmail.com>
2021-10-01 20:59:01 -0300

    Validação de login

--- test/user.test.js
+++ test/user.test.js
@@ -2,6 +2,21 @@ let app = require("../src/app");
 let supertest = require("supertest");
 let request = supertest(app);
 
+let maiUser = {name:"Welyson", email:"welyson12@hotmail.com", password:"123456"};
+
+beforeAll(()=>{
+    return request.post("/user")
+    .send(maiUser)
+    .then(res => {})
+    .catch(err=>{console.log(err)});
+});
+
+afterAll(()=>{
+    return request.delete(`/user/${maiUser.email}`)
+    .then(res => {})
+    .catch(err=>{console.log(err)});
+})
+
 describe("Cadastro de usuário",()=>{
 
     test("Deve cadastrar um usuário com sucesso",()=>{        
@@ -56,5 +71,42 @@ describe("Cadastro de usuário",()=>{
         });
     });
 
+});
 
+describe("Autenticação", ()=>{
+    test("Deve retornar um token quando logar",()=>{
+        return request.post("/auth")
+        .send({email: maiUser.email, password: maiUser.password})
+        .then(res=>{
+            expect(res.statusCode).toEqual(200);
+            expect(res.body.token).toBeDefined();
+        })
+        .catch(err=>{
+            throw new Error(err);
+        });
+    });
+
+    test("Deve impedir que um usuário não cadastrado se logue", ()=>{
+        return request.post("/auth")
+        .send({email: "test@gmail.com", password: "qwerty"})
+        .then(res=>{
+            expect(res.statusCode).toEqual(403);
+            expect(res.body.errors.email).toEqual("E-mail não cadastrado!");
+        })
+        .catch(err=>{
+            throw new Error(err);
+        });
+    });
+
+    test("Deve impedir que um usuário loge com senha errada", ()=>{
+        return request.post("/auth")
+        .send({email: maiUser.email, password: "qwerty"})
+        .then(res=>{
+            expect(res.statusCode).toEqual(403);
+            expect(res.body.errors.password).toEqual("Senha incorreta!");
+        })
+        .catch(err=>{
+            throw new Error(err);
+        });
+    });
 });
